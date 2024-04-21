3099cc3159c0eb9ba97195209ce09f1889630ea0
thelearner411 <91204524+thelearner411@users.noreply.github.com>
2024-04-21 22:31:12 +0200

    homepage

---
+++ google-ai-hackathon-app/app/components/Navigation/Footer.tsx
@@ -0,0 +1,17 @@
+import Link from "next/link";
+
+export default function Footer() {
+    
+    const startYear: number = 2024;
+    const currentYear: number = new Date().getFullYear();
+
+    return (
+        <>
+            <div className={"flex justify-center gap-2 p-5"}>
+                <p>&copy; {currentYear > startYear ?  `${startYear} - ${currentYear}` : `${startYear}`} jenna </p>
+                <span>|</span>
+                <Link href={"/about"}>about</Link>
+            </div>
+        </>
+    );
+}
--- google-ai-hackathon-app/app/page.tsx
+++ google-ai-hackathon-app/app/page.tsx
@@ -1,10 +1,39 @@
 import Image from "next/image";
+import Logo from "@/public/assets/logo-dark.png";
+import Link from "next/link";
+import Footer from "@/app/components/Navigation/Footer"
 
 export default function Home() {
   return (
     <>
-      <div>
-        <p>App</p>
+      <div className={`h-screen flex flex-col justify-between`}>
+
+        <div className={"h-full w-full flex justify-center items-center"}>
+          <div>
+            <Image
+              src={Logo}
+              alt={"Logo"}
+              height={"106"}
+              width={"250"}
+              className={"block m-auto mb-2 h-auto w-[250px]"}
+            />
+
+            <p className={`text-center my-[50px]`}>
+              intelligent literary content generator
+            </p>
+
+            <Link href={"/generate"} 
+            className={"block bg-green-standard px-10 py-5 w-fit m-auto text-center rounded-lg"}>
+              Try
+            </Link>
+          </div>
+
+        </div>
+
+        <div>
+          <Footer />
+        </div>
+
       </div>
     </>
   );
