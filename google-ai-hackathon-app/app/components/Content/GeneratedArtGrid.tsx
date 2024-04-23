36b668668e4b7f63c8b124428008d6a6c11b5d0b
thelearner411 <91204524+thelearner411@users.noreply.github.com>
2024-04-23 15:52:10 +0200

    fix download button position

--- google-ai-hackathon-app/app/components/Content/GeneratedArtGrid.tsx
+++ google-ai-hackathon-app/app/components/Content/GeneratedArtGrid.tsx
@@ -4,6 +4,8 @@ import { GeneratedArtContentSuccess, GeneratedArtGridType } from "@/app/types/Re
 import { FaDownload } from "react-icons/fa";
 
 export default function GeneratedArtGrid({ response_images, orientation }: GeneratedArtGridType) {
+
+    
     return (
         <>
             <div className={"grid grid-col-1 gap-10"}>
@@ -23,7 +25,8 @@ export default function GeneratedArtGrid({ response_images, orientation }: Gener
                                     />
                                 </div>
                                 <div key={`genrated-image-download-icon-container-${index + 1}`}
-                                    className={"absolute bg-green-standard text-black rounded cursor-pointer w-fit p-2"}>
+                                    className={"absolute bottom-0 right-0 m-5 \
+                                    bg-green-standard text-black rounded cursor-pointer w-fit p-2"}>
                                     <FaDownload
                                         key={`genrated-image-download-icon-${index + 1}`}
                                     />
