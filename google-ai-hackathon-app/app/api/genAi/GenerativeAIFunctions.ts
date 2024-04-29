1222803f245b11e894c3893523947713b8d734a0
thelearner411 <91204524+thelearner411@users.noreply.github.com>
2024-04-29 18:26:42 +0200

    create node functions for generating text content with only text prompt and with text and image prompt

---
+++ google-ai-hackathon-app/app/api/genAi/GenerativeAIFunctions.ts
@@ -0,0 +1,123 @@
+import {
+    FunctionDeclarationSchemaType,
+    HarmBlockThreshold,
+    HarmCategory,
+    VertexAI
+} from '@google-cloud/vertexai';
+import { GoogleGenerativeAI } from "@google/generative-ai";
+import { base64ToJSON } from "@/app/utils/DataParsing"
+import { getImageTmpPathFromFile, filePathToBase64 } from "@/app/utils/Files"
+
+
+// Intialise constants to run Generative AI models
+const project = process.env.GCP_PROJECT_ID!;
+const location = process.env.GCP_LOCATION!;
+const saJson = base64ToJSON(process.env.GCP_SA_KEY_STRING!);
+const textModel = 'gemini-1.0-pro';
+const visionModel = 'gemini-1.0-pro-vision';
+
+// Initialise Google Authentication credentials library
+const authOptions = {
+    credentials: {
+        client_email: saJson.client_email,
+        private_key: saJson.private_key,
+    }
+}
+
+// Initialise Vertex AI Object
+const vertexAI = new VertexAI({ project: project, location: location, googleAuthOptions: authOptions });
+const generativeModelText = vertexAI.getGenerativeModel({
+    model: textModel,
+    safetySettings: [{ category: HarmCategory.HARM_CATEGORY_DANGEROUS_CONTENT, threshold: HarmBlockThreshold.BLOCK_MEDIUM_AND_ABOVE }],
+    generationConfig: { maxOutputTokens: 256 },
+});
+
+const genAI = new GoogleGenerativeAI(process.env.GEMINI_API_KEY!);
+const textFromTextAndImageModel = genAI.getGenerativeModel({ model: "gemini-pro-vision" });
+
+
+// Function to generate text from only text prompt
+export const generateTextFromTextPrompt = async (
+    contentType: string,
+    prompt: string
+) => {
+
+    try {
+        // Construct string for final prompt with content type and user input prompt text
+        const finalPrompt: string = `Write a ${contentType} with the following prompt: ${prompt}.
+        Include an appropriate title in bold for the content generated in the final response.`
+
+        // Build content parameters for generative AI model
+        let requestContents: any = {
+            contents: [{ role: 'user', parts: [{ text: finalPrompt }] }],
+        };
+        // Get response for prompt
+        const response = await generativeModelText.generateContent(requestContents);
+
+        // Initialise final response as error
+        let finalResponse: any = {
+            "error": "Something went wrong."
+        };
+
+        // Change final response to successful response if text is obtained
+        if (response.response.candidates && response.response.candidates[0].content.parts[0].text) {
+            finalResponse = {
+                "response": response.response.candidates[0].content.parts[0].text
+            };
+        }
+
+        return finalResponse;
+
+    } catch (error: any) {
+        // Return error response
+        return { "error": error.message }
+    }
+}
+
+// Function to generate text from text and image prompt (multimodal model)
+export const generateTextFromTextAndImagePrompt = async (
+    contentType: string,
+    prompt: string,
+    image: File,
+) => {
+    try {
+
+        // Write image to location in /tmp
+        const tmpImage = await getImageTmpPathFromFile(image);
+
+        // If tmp image is successful continue with content generation
+        if (tmpImage && "imageUUID" in tmpImage && "imageName" in tmpImage && "imagePath" in tmpImage) {
+
+            // Convert image to base 64 string
+            const fileBase64String = filePathToBase64(tmpImage.imagePath);
+
+            // Create file parts for prompt
+            const filePart: any = { inline_data: { data: fileBase64String, mimeType: image.type } };
+            const imageParts: any = [filePart];
+
+            // Construct final content generation prompt with content type and user input propt text
+            const final_prompt: string = `Write a ${contentType} about this image with the following prompt: ${prompt}. 
+            Include an appropriate title in bold for the content generated in the final response.`;
+            const result = await textFromTextAndImageModel.generateContent([final_prompt, ...imageParts]);
+            const response = await result.response;
+
+            // Return successful response
+            return {
+                "response": response.text()
+            }
+        }
+        // Return error response
+        return { "error": "Error generating text from text and image and prompt." };
+    } catch (error: any) {
+
+        // Return warning if user is located some place where Gemini API is unavailable
+        if (error.message === "[GoogleGenerativeAI Error]: Error fetching from https://generativelanguage.googleapis.com/v1beta/models/gemini-pro-vision:generateContent: [400 Bad Request] User location is not supported for the API use.") {
+            return {
+                "warnings": ["Sorry. The API used for this prompt is not available in your location. Try using a VPN to connect to a foreign network."]
+            };
+        }
+
+        // Return error response
+        return { "error": error.message };
+    }
+}
--- google-ai-hackathon-app/app/utils/Files.tsx
+++ google-ai-hackathon-app/app/utils/Files.tsx
@@ -1,6 +1,7 @@
 import fs from "fs";
 import { uuidv7 } from "uuidv7";
 
+// Function to accept base64String, convert it to a file and write it to /tmp
 export const getImageTmpPathFromBase64String =  (base64String: string) => {
     // Convert base64 image string to a buffer
     const imageData: any = Buffer.from(base64String, 'base64');
@@ -19,9 +20,10 @@ export const getImageTmpPathFromBase64String =  (base64String: string) => {
     };
 }
 
+// Function to accept file and write it to /tmp
 export const getImageTmpPathFromFile = async(imageFile: any) => {
 
-    // Convert base64 image file to a buffer
+    // Convert image file to a buffer
     const imageData: any = Buffer.from(await imageFile.arrayBuffer());
 
     // Initialise id, file name and file path at /tmp for image
@@ -36,4 +38,32 @@ export const getImageTmpPathFromFile = async(imageFile: any) => {
         imageName: tmpImageName,
         imagePath: tmpImagePath,
     };
+}
+
+// Functon to accept filePath and convert it to base64 string
+export const filePathToBase64 = (filePath: string) => {
+    const fileData = fs.readFileSync(filePath);    
+    const base64String = fileData.toString('base64');
+    return base64String;
+}
+
+// Function to check for image size and mime type warnings
+export const checkImageCompatibility = (image: File) => {
+    const warnings: string[] = [];
+
+    // Check for appropriate file extension
+    const imageTypeExploded: string[] = image.type.split("/");
+    const fileExtension: string = imageTypeExploded[imageTypeExploded.length - 1];
+    const allowedFileExtensions: string[] = ["jpeg", "jpg", "png"];
+    if(!allowedFileExtensions.includes(fileExtension)){
+        warnings.push("Only images with extensions jpeg, jpg and png are allowed.");
+    }
+
+    // Check for appropriate file size
+    const file_size: number = image.size;
+    if(file_size >= 27000000){
+        warnings.push("File size is too large. Choose a file of a size lower than 20 MB.")
+    }
+
+    return warnings;
 }
